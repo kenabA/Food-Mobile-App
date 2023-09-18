aee558800d8c5c3a4c5195e9d4fadbe4b3a30fb9
kenabey <kebab.bahadur@gmail.com>
2023-09-18 20:16:56 +0545

    updates files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
-const para = Array.from(document.getElementsByTagName("p"));
-para.forEach(function (para) {
-  if (para.textContent == "AVAILABLITY" || para.textContent == "availablity") {
-    para.style.marginBottom = "6px";
-  }
-});
+fetch(
+  "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+)
+  .then(function (response) {
+    if (response.status === 200) {
+      return response.json();
+    } else {
+      alert("Incorrect Call");
+    }
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    alert(`Unfortunate error: ${error}`);
+  });
