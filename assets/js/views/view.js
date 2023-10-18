faad74174c751a2402c8826cd336004fe8a0a05d
kenabey <kebab.bahadur@gmail.com>
2023-10-18 22:07:23 +0545

    added files

--- assets/js/views/view.js
+++ assets/js/views/view.js
@@ -1,9 +1,12 @@
+const resultsContainer = document.querySelector("#results");
 export default class View {
-  _errorMessage = "NO RECIPES FOUND";
+  _errorMessage = "!! NO RECIPES FOUND !!";
+
   render(data) {
     this._data = data;
     console.log("Yo");
     if (this._data.length == 0) {
+      resultsContainer.style.overflow = "hidden";
       return this.renderError(this._errorMessage);
     }
     this._clear();
@@ -28,7 +31,7 @@ export default class View {
   }
 
   renderError(msg) {
-    const markup = `<p class="text-danger font-18 fw-medium text-center h-100 d-flex align-items-center justify-content-center">${msg}</p>`;
+    const markup = `<p class="text-black font-18 fw-medium text-center h-100 d-flex align-items-top justify-content-center">${msg}</p>`;
     this._clear();
     this._parentEl.insertAdjacentHTML("afterbegin", markup);
   }
